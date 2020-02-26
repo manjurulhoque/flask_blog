6c1c0b7ec6b55340eb0eb1a0b09a3ac0787c082b
Manjurul Hoque Rumi <rumimanzurulhoque@gmail.com>
2020-02-27 00:45:47 +0600

    Password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,7 +1,10 @@
+import os
+
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
@@ -13,5 +16,11 @@ login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message = 'Please login to access this page'
 login_manager.login_message_category = 'info'
+app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '1f9f669a19e380'
+app.config['MAIL_PASSWORD'] = '72e75a4f18d6ae'
+app.config['MAIL_USE_TLS'] = True
+mail = Mail(app)
 
 from flaskblog import routes
